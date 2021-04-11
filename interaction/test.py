001b783cb6c1eb0a1b3378f5aada13541c0280bc
Artemiy Klyachin <artklyachin@gmail.com>
2021-04-11 17:59:22 +0300

    add interaction

---
+++ interaction/test.py
@@ -0,0 +1,72 @@
+import requests
+
+def create_game(username):
+    try:
+        d = requests.post("http://tp-project2021.herokuapp.com/api/v1/game_lobby/create_game", json={"username": username}).json()
+        return {"cfg": d["cfg"], "game": d["game"], "user": d["user"]}
+    except Exception as e:
+        print("exception in create_game: " + str(e))
+        return {}
+
+def fetch_game(session_token, game_id): #выводит информацию по игре
+    try:
+        d = requests.get("http://tp-project2021.herokuapp.com/api/v1/game_lobby/fetch_game", headers={"Authorization":session_token, "Game":game_id}).json()
+        return {"game": d["game"], "user": d["user"]}
+    except Exception as e:
+        print("exception in fetch_game: " + str(e))
+        return {}
+
+def update_ready(session_token, game_id):
+    try:
+        d = requests.get("http://tp-project2021.herokuapp.com/api/v1/game_lobby/update_ready", headers={"Authorization":session_token, "Game":game_id}).json()
+        return {"game": d["game"], "user": d["user"]}
+    except Exception as e:
+        print("exception in update_ready: " + str(e))
+        return {}
+
+def leave_game(session_token, game_id):
+    try:
+        d = requests.get("http://tp-project2021.herokuapp.com/api/v1/game_lobby/leave_game", headers={"Authorization":session_token, "Game":game_id}).json()
+        return {"game": d["game"]}
+    except Exception as e:
+        print("exception in leave_game: " + str(e))
+        return {}
+
+def join_game(ref_code, username):
+    try:
+        d = requests.get("http://tp-project2021.herokuapp.com/api/v1/game_lobby/join_game", params={"ref_code":ref_code, "username":username}).json()
+        return {"cfg": d["cfg"], "game": d["game"], "user": d["user"]}
+    except Exception as e:
+        print("exception in join_game: " + str(e))
+        return {}
+
+def start_game(session_token, game_id): #подключение к игре
+    try:
+        d = requests.get("http://tp-project2021.herokuapp.com/api/v1/game_lobby/start_game", headers={"Authorization":session_token, "Game":game_id}).json()
+        if ("message" in d):
+            return False
+        else:
+            return True
+    except Exception as e:
+        print("exception in start_game: " + str(e))
+        return False
+
+def get_authorization_data(t):
+    return (t["user"]["session_token"], t["game"]["_id"])
+
+player1 = create_game("artemiy")
+player2 = (join_game(player1["game"]["ref_code"], "artemiy2"))
+a1 = get_authorization_data(player1)
+a2 = get_authorization_data(player2)
+print(fetch_game(a1[0], a1[1]))
+print(fetch_game(a2[0], a2[1]))
+print(update_ready(a1[0], a1[1]))
+print(update_ready(a2[0], a2[1]))
+if (start_game(a1[0], a1[1])): #без разницы кто, но только одного
+    print("YES!")
+else:
+    print("NO")
+print(leave_game(a1[0], a1[1]))
+print(leave_game(a2[0], a2[1]))
+
+
